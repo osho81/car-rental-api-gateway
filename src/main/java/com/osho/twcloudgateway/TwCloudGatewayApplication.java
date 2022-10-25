0f8e3bf61d18882806966cd7b984a68f20df6209
osho81 <ossi.hour@gmail.com>
2022-10-26 00:25:09 +0200

    Add files

---
+++ src/main/java/com/osho/twcloudgateway/TwCloudGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.osho.twcloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient // Manually added
+@SpringBootApplication
+public class TwCloudGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwCloudGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/osho/twcloudgateway/TwCloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osho.twcloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwCloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
