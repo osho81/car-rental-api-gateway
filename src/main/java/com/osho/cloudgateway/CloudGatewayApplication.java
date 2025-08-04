e20024513c71108b2b0b481c653a1b082459b07e
osho81 <ossi.hour@gmail.com>
2025-08-04 21:26:46 +0200

    Refactored

--- src/main/java/com/osho/cloudgateway/CloudGatewayApplication.java
+++ src/main/java/com/osho/cloudgateway/CloudGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.osho.twcloudgateway;
+package com.osho.cloudgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/osho/cloudgateway/ApiGatewayApplicationTests.java
+++ src/test/java/com/osho/cloudgateway/ApiGatewayApplicationTests.java
@@ -1,10 +1,10 @@
-package com.osho.twcloudgateway;
+package com.osho.cloudgateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CloudGatewayApplicationTests {
+class ApiGatewayApplicationTests {
 
 	@Test
 	void contextLoads() {
